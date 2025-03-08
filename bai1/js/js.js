508dc4ec741158f84a0a1a3546092d5d01b663bd
phudammelaptrinh <phuho22112003@gmail.com>
2025-03-08 14:36:16 +0700

    var-let su khac biet

---
+++ bai1/js/js.js
@@ -0,0 +1,33 @@
+const x =5 ;
+
+document.getElementById("demo1").innerHTML="JS";
+
+function myFunction1(){
+  document.getElementById("demo1").innerHTML="Hi toi den tu vung tau, va toi rat thich hoc JS";
+};
+
+let x1,y1,z1;
+x1=5;
+y1=6;
+z1=x1+y1;
+document.getElementById("demo3").innerHTML = " Tong cua x1 va y1 la :"+z1 + ".";
+
+//numbers 
+let chieucao = 160;
+let cannang = 50;
+
+let BMI= cannang/(chieucao*chieucao)
+console.log('BMI cua co ay la '+ BMI);
+
+//Strings 
+let color = " Yellow ";
+let lastName = " Phu ";
+let car = " BMW ";
+
+console.log("Toi thich xe " + car + "voi mau xe "+ color + "va ten cua toi la " + lastName);
+
+//booleans 
+let x2 = true;
+let y2 = false;
+
+
