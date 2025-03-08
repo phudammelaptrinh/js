35da4ecaebec53f1fa88a572c69fc38022a2dab2
phudammelaptrinh <phuho22112003@gmail.com>
2025-03-08 21:06:00 +0700

    cach su dung object

--- bai1/js/js.js
+++ bai1/js/js.js
@@ -1,54 +1,12 @@
-// const x =5 ;
-
-// document.getElementById("demo1").innerHTML="JS";
-
-// function myFunction1(){
-//   document.getElementById("demo1").innerHTML="Hi toi den tu vung tau, va toi rat thich hoc JS";
-// };
-
-// let x1,y1,z1;
-// x1=5;
-// y1=6;
-// z1=x1+y1;
-// document.getElementById("demo3").innerHTML = " Tong cua x1 va y1 la :"+z1 + ".";
-
-// //numbers 
-// let chieucao = 160;
-// let cannang = 50;
-
-// let BMI= cannang/(chieucao*chieucao)
-// console.log('BMI cua co ay la '+ BMI);
-
-// //Strings 
-// let color = " Yellow ";
-// let lastName = " Phu ";
-// let car = " BMW ";
-
-// console.log("Toi thich xe " + car + "voi mau xe "+ color + "va ten cua toi la " + lastName);
-
-// //booleans 
-// let x2 = true;
-// let y2 = false;
-// console(x2 && y2);
-// console(x2 || y2);
-
-// //arrays
-// let cars = ["BMW","Volvo","Ford"];
-// console.log(cars[0]);
+// 
 
 
-// let x = myTest(4,3);
-// document.getElementById("demo4").innerHTML = x;
-
-
-// function myTest(a,b){
-//   return a*b;
-// }
-
-let x = myTest(5,3);
-document.getElementById("demo3").innerHTML = x; 
-
-function myTest(a,b){
-return a*b;
+const people = {
+  ten: "Nguyen Van B",
+  tuoi: 20,
+  gioiTinh: "Nam",
+  diaChi: "Sai Gon"
 }
+//hien thi data from object 
 
+document.getElementById("demo3").innerHTML = 'Day la thong tin gioi thieu ve mot nguoi co ten ' + people.ten + ' ' + people.tuoi + ' ' + people.gioiTinh + ' ' + people.diaChi; 
