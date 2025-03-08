0ce7c7f8138bee39450c9000e1aa6755cf1ea565
phudammelaptrinh <phuho22112003@gmail.com>
2025-03-08 09:00:18 +0700

    upload ngay 8-3-25

---
+++ js/js.js
@@ -0,0 +1,7 @@
+const x =5 ;
+
+document.getElementById("demo1").innerHTML="JS";
+
+function myFunction1(){
+  document.getElementById("demo1").innerHTML="Hi toi den tu vung tau, va toi rat thich hoc JS";
+}
