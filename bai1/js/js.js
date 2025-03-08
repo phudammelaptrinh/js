19a802c92cc0c7cbf35c757d261ed1bff05954a1
phudammelaptrinh <phuho22112003@gmail.com>
2025-03-08 21:09:45 +0700

    hieu ve project

--- bai1/js/js.js
+++ bai1/js/js.js
@@ -1,12 +1,23 @@
-// 
+// // 
 
 
-const people = {
-  ten: "Nguyen Van B",
-  tuoi: 20,
-  gioiTinh: "Nam",
-  diaChi: "Sai Gon"
-}
-//hien thi data from object 
+// const people = {
+//   ten: "Nguyen Van B",
+//   tuoi: 20,
+//   gioiTinh: "Nam",
+//   diaChi: "Sai Gon"
+// }
+// //hien thi data from object 
 
-document.getElementById("demo3").innerHTML = 'Day la thong tin gioi thieu ve mot nguoi co ten ' + people.ten + ' ' + people.tuoi + ' ' + people.gioiTinh + ' ' + people.diaChi; 
+// document.getElementById("demo3").innerHTML = 'Day la thong tin gioi thieu ve mot nguoi co ten ' + people.ten + ' ' + people.tuoi + ' ' + people.gioiTinh + ' ' + people.diaChi; 
+
+
+const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+
+//hien thi object content 
+document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " " + person.eyeColor ;
