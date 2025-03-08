fe062d106bd51281c58d829ba636479ea041d220
phudammelaptrinh <phuho22112003@gmail.com>
2025-03-08 20:33:11 +0700

    cach-su-dung-ham

--- bai1/js/js.js
+++ bai1/js/js.js
@@ -1,33 +1,54 @@
-const x =5 ;
+// const x =5 ;
 
-document.getElementById("demo1").innerHTML="JS";
+// document.getElementById("demo1").innerHTML="JS";
 
-function myFunction1(){
-  document.getElementById("demo1").innerHTML="Hi toi den tu vung tau, va toi rat thich hoc JS";
-};
+// function myFunction1(){
+//   document.getElementById("demo1").innerHTML="Hi toi den tu vung tau, va toi rat thich hoc JS";
+// };
 
-let x1,y1,z1;
-x1=5;
-y1=6;
-z1=x1+y1;
-document.getElementById("demo3").innerHTML = " Tong cua x1 va y1 la :"+z1 + ".";
+// let x1,y1,z1;
+// x1=5;
+// y1=6;
+// z1=x1+y1;
+// document.getElementById("demo3").innerHTML = " Tong cua x1 va y1 la :"+z1 + ".";
 
-//numbers 
-let chieucao = 160;
-let cannang = 50;
+// //numbers 
+// let chieucao = 160;
+// let cannang = 50;
 
-let BMI= cannang/(chieucao*chieucao)
-console.log('BMI cua co ay la '+ BMI);
+// let BMI= cannang/(chieucao*chieucao)
+// console.log('BMI cua co ay la '+ BMI);
 
-//Strings 
-let color = " Yellow ";
-let lastName = " Phu ";
-let car = " BMW ";
+// //Strings 
+// let color = " Yellow ";
+// let lastName = " Phu ";
+// let car = " BMW ";
 
-console.log("Toi thich xe " + car + "voi mau xe "+ color + "va ten cua toi la " + lastName);
+// console.log("Toi thich xe " + car + "voi mau xe "+ color + "va ten cua toi la " + lastName);
 
-//booleans 
-let x2 = true;
-let y2 = false;
+// //booleans 
+// let x2 = true;
+// let y2 = false;
+// console(x2 && y2);
+// console(x2 || y2);
 
+// //arrays
+// let cars = ["BMW","Volvo","Ford"];
+// console.log(cars[0]);
+
+
+// let x = myTest(4,3);
+// document.getElementById("demo4").innerHTML = x;
+
+
+// function myTest(a,b){
+//   return a*b;
+// }
+
+let x = myTest(5,3);
+document.getElementById("demo3").innerHTML = x; 
+
+function myTest(a,b){
+return a*b;
+}
 
