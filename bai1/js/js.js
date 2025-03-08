4a73ba1adad6b51e2e9217e609d3f53d932fb30e
phudammelaptrinh <phuho22112003@gmail.com>
2025-03-09 06:14:05 +0700

    object ngay9-3

--- bai1/js/js.js
+++ bai1/js/js.js
@@ -1,34 +1,62 @@
-// // // 
+// // // // // // 
 
 
-// // const people = {
-// //   ten: "Nguyen Van B",
-// //   tuoi: 20,
-// //   gioiTinh: "Nam",
-// //   diaChi: "Sai Gon"
+// // // // // const people = {
+// // // // //   ten: "Nguyen Van B",
+// // // // //   tuoi: 20,
+// // // // //   gioiTinh: "Nam",
+// // // // //   diaChi: "Sai Gon"
+// // // // // }
+// // // // // //hien thi data from object 
+
+// // // // // document.getElementById("demo3").innerHTML = 'Day la thong tin gioi thieu ve mot nguoi co ten ' + people.ten + ' ' + people.tuoi + ' ' + people.gioiTinh + ' ' + people.diaChi; 
+
+
+// // // // const person = new Object();
+// // // // person.firstName = "John";
+// // // // person.lastName = "Doe";
+// // // // person.age = 50;
+// // // // person.eyeColor = "blue";
+
+
+// // // // //hien thi object content 
+// // // // document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " " + person.eyeColor ;
+
+// // // //create an object 
+// // // const person = new Object();
+// // // person.firstName = "Phu";
+// // // person.lastName = "Ho";
+// // // person.age = 50;
+// // // person.eyeColor = "brown";
+
+
+// // // //xoa 
+// // // delete person.age;
+// // // //hien thi object content
+// // // document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " years old.";
+
+// // const myObj ={ 
+// //   name : "John",
+// //   age : 30,
+// //   myCars : {
+// //     car1 : "Ford",
+// //     car2 : "BMW",
+// //     car3 : "Fiat"
+// //   }
 // // }
-// // //hien thi data from object 
 
-// // document.getElementById("demo3").innerHTML = 'Day la thong tin gioi thieu ve mot nguoi co ten ' + people.ten + ' ' + people.tuoi + ' ' + people.gioiTinh + ' ' + people.diaChi; 
+// // //hien thi 
+// // document.getElementById("demo3").innerHTML =myObj.myCars.car3
 
+// const toi = {
+//   ten : "Phu",
+//   age : 30,
+//   xe :{
+//     xe1: "honda future",
+//     xe2 : "yamaha exciter",
+//     xe3 : "yamaha jupiter"
+//   }
+// }
 
-// const person = new Object();
-// person.firstName = "John";
-// person.lastName = "Doe";
-// person.age = 50;
-// person.eyeColor = "blue";
-
-
-// //hien thi object content 
-// document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " " + person.eyeColor ;
-
-//create an object 
-const person = new Object();
-person.firstName = "Phu";
-person.lastName = "Ho";
-person.age = 50;
-person.eyeColor = "brown";
-
-//hien thi object content
-document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " years old.";
+// document.getElementById("demo3").innerHTML = "Toi ten la :" + toi.ten + " voi" + toi.age +  " tuoi " + " va so thich la di xe " + toi.xe.xe1 + " va " + toi.xe.xe2 + " va " + toi.xe.xe3;
 
