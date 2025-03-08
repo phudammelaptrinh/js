1270ae33134c9078ca69ab1321381f44f056864a
phudammelaptrinh <phuho22112003@gmail.com>
2025-03-09 06:04:38 +0700

    object 9-3

--- bai1/js/js.js
+++ bai1/js/js.js
@@ -1,23 +1,34 @@
-// // 
+// // // 
 
 
-// const people = {
-//   ten: "Nguyen Van B",
-//   tuoi: 20,
-//   gioiTinh: "Nam",
-//   diaChi: "Sai Gon"
-// }
-// //hien thi data from object 
+// // const people = {
+// //   ten: "Nguyen Van B",
+// //   tuoi: 20,
+// //   gioiTinh: "Nam",
+// //   diaChi: "Sai Gon"
+// // }
+// // //hien thi data from object 
 
-// document.getElementById("demo3").innerHTML = 'Day la thong tin gioi thieu ve mot nguoi co ten ' + people.ten + ' ' + people.tuoi + ' ' + people.gioiTinh + ' ' + people.diaChi; 
+// // document.getElementById("demo3").innerHTML = 'Day la thong tin gioi thieu ve mot nguoi co ten ' + people.ten + ' ' + people.tuoi + ' ' + people.gioiTinh + ' ' + people.diaChi; 
 
 
+// const person = new Object();
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+
+// //hien thi object content 
+// document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " " + person.eyeColor ;
+
+//create an object 
 const person = new Object();
-person.firstName = "John";
-person.lastName = "Doe";
+person.firstName = "Phu";
+person.lastName = "Ho";
 person.age = 50;
-person.eyeColor = "blue";
+person.eyeColor = "brown";
 
+//hien thi object content
+document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " years old.";
 
-//hien thi object content 
-document.getElementById("demo3").innerHTML = person.firstName + " is " + person.age + " " + person.eyeColor ;
